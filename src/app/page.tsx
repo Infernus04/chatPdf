adfc57f4abe203bd0f5413d55e0dec19e9c016fc
Ishan <magardeishan@gmail.com>
2024-12-18 20:49:18 +0530

    implement drizzle orm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import {LogIn} from "lucide-react"
+import Fileupload from "@/components/Fileupload";
 
 export default async function Home() {
   const { userId } = await auth();
@@ -12,20 +13,20 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center justify-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-4 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSwitchSessionUrl="/" />
           </div>
-          <div className="flex mt-8">
+          <div className="flex mt-2">
             {isAuth && <Button>Go to Chats</Button>}
           </div>
 
-          <p className="max-w-lg mt-0 ml-6 text-lg text-slate-600">
+          <p className="max-w-lg mt-0 ml-8 text-lg text-slate-600">
             Join millions of students, researchers and professionals to
             instantly answer questions and understand research with AI
           </p>
 
           <div className="max-w-lg mt-4 mr-4">
-            {isAuth ? (<h1>fileupload</h1>) : (
+            {isAuth ? (<Fileupload/>) : (
               <Link href= "/sign-in">
                 <Button>Login to get started
                   <LogIn className="w-4 h-4 "/>
---
+++ src/components/Fileupload.tsx
@@ -0,0 +1,15 @@
+"use-client";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+
+const Fileupload = () => {
+  return (
+  <div className="p-2 bg-white rounded-xl">
+
+    <div>
+        <input/>
+    </div>
+  </div>);
+};
+
+export default Fileupload;
